1150eaf90169c00f0260c2b8760c6315b4267a44
Janki Gandhi <jankigandhi@atyantik.com>
2018-06-29 00:09:42 +0530

    designing about and service section

--- beta/inc/head.php
+++ beta/inc/head.php
@@ -7,7 +7,8 @@
 		<meta name="description" content="Eventerprise">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" href="../../../images/favicon.ico" />
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,300italic,400italic,600italic,700,700italic,800,800italic' rel='stylesheet' type="text/css" />
 		<link href="../../libs/font-awesome.min.css" rel="stylesheet" type="text/css" />
+		<link href="../../libs/themify-icons.css" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="../../css/style.css">
 		<link rel="stylesheet" href="../../inc/header/css/header.css">
--- beta/pages/home/home.php
+++ beta/pages/home/home.php
@@ -2,31 +2,185 @@
 <link rel="stylesheet" type="text/css" href="css/home.css">
 </head>
 <body>
-	<div class="row flex-xl-nowrap mx-0">
-		<?php include_once '../../inc/header/header.php';?>
-		<main class="col-12 col-md-9 col-xl-10 p-0">
-			<!-- banner-section -->
-			<div class="banner-section">
-				<div class="bg-img" style="background-image: url('../../../images/bg.jpg');">
-					<div class="content">
-						<h1 class="text-uppercase text-white">Hello I 'm 
-							<span class="d-block">
-								Sam Martin
-							</span>
-						</h1>
-						<div class="h1 typing-text text-uppercase text-white">Frontend developer</div>
-						<button class="btn btn-success text-uppercase px-4">Download my cv</button>
+<div class="row flex-xl-nowrap mx-0">
+	<?php include_once '../../inc/header/header.php';?>
+	<main class="col-12 col-md-9 col-xl-10 p-0">
+		<!-- banner-section -->
+		<div class="banner-section">
+			<div class="bg-img" style="background-image: url('../../../images/bg.jpg');">
+				<div class="content">
+					<h1 class="text-uppercase text-white">Hello I 'm
+					<span class="d-block">
+						Sam Martin
+					</span>
+					</h1>
+					<div class="h1 typing-text text-uppercase text-white">Frontend developer</div>
+					<button class="btn btn-success text-uppercase px-4">Download my cv</button>
+				</div>
+				<a href="#about" class="scroll-down">
+					<span></span>
+				</a>
+			</div>
+		</div>
+		<!-- about section -->
+		<div id="about" class="py-5 px-md-5 px-3">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="about-img">
+						<img src="../../../images/me.jpg" alt="User" class="mw-100" />
 					</div>
-					<a href="#about" class="scroll-down">
-						<span></span>
-					</a>
+					<div class="about-social bg-success text-center p-3 text-center">
+						<a href="#" class="social-icon d-inline-flex align-items-center justify-content-center m-1">
+							<i class="fa fa-facebook"></i>
+						</a>
+						<a href="#" class="social-icon d-inline-flex align-items-center justify-content-center m-1">
+							<i class="fa fa-twitter"></i>
+						</a>
+						<a href="#" class="social-icon d-inline-flex align-items-center justify-content-center m-1">
+							<i class="fa fa-dribbble"></i>
+						</a>
+						<a href="#" class="social-icon d-inline-flex align-items-center justify-content-center m-1">
+							<i class="fa fa-vimeo"></i>
+						</a>
+						<a href="#" class="social-icon d-inline-flex align-items-center justify-content-center m-1">
+							<i class="fa fa-pinterest-p"></i>
+						</a>
+						<a href="#" class="social-icon d-inline-flex align-items-center justify-content-center m-1">
+							<i class="fa fa-behance"></i>
+						</a>
+						<a href="#" class="social-icon d-inline-flex align-items-center justify-content-center m-1">
+							<i class="fa fa-linkedin"></i>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-8">
+					<div class="bg-success py-2 px-4 d-inline-block text-white mb-3">Know about me</div>
+					<h2>About Me</h2>
+					<div class="row mx-0 my-4">
+						<div class="col-lg-4 col-md-6 px-md-0 py-3">
+							<div class="icons d-inline-block align-top pr-3">
+								<i class="ti-user"></i>
+							</div>
+							<div class="d-inline-block align-top">
+								<h5 class="font-weight-medium">Name:</h5>
+								<p class="text-gray-light font-weight-medium">Sam Martin</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 px-md-0 py-3">
+							<div class="icons d-inline-block align-top pr-3">
+								<i class="ti-email"></i>
+							</div>
+							<div class="d-inline-block align-top">
+								<h5 class="font-weight-medium">Email:</h5>
+								<p class="text-gray-light font-weight-medium">sam@website.com</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 px-md-0 py-3">
+							<div class="icons d-inline-block align-top pr-3">
+								<i class="ti-mobile"></i>
+							</div>
+							<div class="d-inline-block align-top">
+								<h5 class="font-weight-medium">Phone:</h5>
+								<p class="text-gray-light font-weight-medium">(123)-456-7890</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 px-md-0 py-3">
+							<div class="icons d-inline-block align-top pr-3">
+								<i class="ti-calendar"></i>
+							</div>
+							<div class="d-inline-block align-top">
+								<h5 class="font-weight-medium">Date Of Birth:</h5>
+								<p class="text-gray-light font-weight-medium">
+									25 September
+									<span class="d-block">1990</span>
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 px-md-0 py-3">
+							<div class="icons d-inline-block align-top pr-3">
+								<i class="ti-direction-alt"></i>
+							</div>
+							<div class="d-inline-block align-top">
+								<h5 class="font-weight-medium">Address:</h5>
+								<p class="text-gray-light font-weight-medium">
+									24 SL Road,
+									<span class="d-block">London. UK</span>
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 px-md-0 py-3">
+							<div class="icons d-inline-block align-top pr-3">
+								<i class="ti-flag-alt-2"></i>
+							</div>
+							<div class="d-inline-block align-top">
+								<h5 class="font-weight-medium">Nationality:</h5>
+								<p class="text-gray-light font-weight-medium">
+									United Kingdom
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="desc">
+						<p class="font-italic text-gray-light">I have more than <span class="text-dark font-weight-medium">9 years of experience</span> in the field of Graphic/ E Learning/Web Designing. Specialized in Adobe web & graphic designing tools and also in otheta tools. Professional in Corporate branding, Graphic designing, <span class="text-dark font-weight-medium">Web Designing,</span> visualization, GUI, graphics & animations for e-learning, illustrations, web icons, flash web banner, flash intro animations, logos, brochures, posters etc.</p>
+						<p class="font-italic text-gray-light">Creative Solution Provider as a Freelance Graphic Artist and a Dedicated, Team worker to Induce the Creative Juice among the colleagues with a View to deliver the Best and New in the business.</p>
+						<div class="text-right py-3">
+							<img src="../../../images/signature.png" class="mw-100" />
+						</div>
+					</div>
 				</div>
 			</div>
-			<!-- about section -->
-			<div id="about" class="text-center py-5 h2" style="height: 400px;">About section</div>
-
-		</main>
-		<?php include_once '../../inc/footer/footer.php';?>
-	</div>
+		</div>
+		<!-- service section -->
+		<div id="services" class="py-5 px-md-5 px-3">
+			<div class="bg-success py-2 px-4 d-inline-block text-white mb-3">What i can do better</div>
+			<h2>Services</h2>
+			<div class="row my-4">
+				<div class="col-lg-3 col-md-6">
+					<div class="service-block">
+						<span class="service-icon text-gray-light ti-desktop"></span>
+						<h4 class="my-4">Tranding Design</h4>
+						<p class="text-gray-light">You will begin to realise why this exercise is called the Dickens Pattern (with reference to the ghost showing Scrooge some different futures) as you notice that the idea of this exercise is to hypnotize yourself to be aware of two....</p>
+						<div class="contact-btn">
+							<a href="#" class="btn btn-success btn-block rounded-0">Read More...</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="service-block">
+						<span class="service-icon text-gray-light ti-slice"></span>
+						<h4 class="my-4">Clear Code</h4>
+						<p class="text-gray-light">Two distinct pathways that you could take for your life this very day. Have a think about something that you do that you are maybe not motivated to change about yourself. Prior to running through this technique, just have a about...</p>
+						<div class="contact-btn">
+							<a href="#" class="btn btn-success btn-block rounded-0">Read More...</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="service-block">
+						<span class="service-icon text-gray-light ti-brush-alt"></span>
+						<h4 class="my-4">App Design</h4>
+						<p class="text-gray-light">Then with that thing in mind, follow these simple steps. Step One: Get yourself nice and relaxed and settled. Concentrate on your breathing, engage in the moment and spend some time being still, quiet and drift inside of your mind... </p>
+						<div class="contact-btn">
+							<a href="#" class="btn btn-success btn-block rounded-0">Read More...</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="service-block">
+						<span class="service-icon text-gray-light ti-layout-media-center"></span>
+						<h4 class="my-4">UI/UX Design</h4>
+						<p class="text-gray-light">Become aware of the temperature, the sights, the sounds and enjoy walking along the path of your life. Make it sensory rich and get comfortable with the idea. Imagine the feeling of your feet walking along the path and the sound....</p>
+						<div class="contact-btn">
+							<a href="#" class="btn btn-success btn-block rounded-0">Read More...</a>
+						</div>
+					</div>
+				</div>
+				
+				
+			</div>
+		</div>
+	</main>
+	<?php include_once '../../inc/footer/footer.php';?>
+</div>
 </body>
 </html>
