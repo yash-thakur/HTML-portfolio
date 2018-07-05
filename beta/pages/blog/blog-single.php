a72025ac9b54c859ab03397e036114a6dd140e9b
Janki Gandhi <jankigandhi@atyantik.com>
2018-07-05 23:31:53 +0530

    designing single blog page

---
+++ beta/pages/blog/blog-single.php
@@ -0,0 +1,141 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" type="text/css" href="css/blog.css">
+</head>
+<body>
+<div class="row flex-xl-nowrap mx-0">
+	<?php include_once '../../inc/header/header.php';?>
+	<main class="col-12 col-lg-9 col-xl-10 p-0">
+		<!-- blog-banner-section -->
+		<div class="blog-bg  gradient" style="background-image: url('../../../images/bg/01.jpg')">
+			<div class="blog-content container">
+				<div class="top-bar d-flex justify-content-between mx-4 py-5">
+					<a class="btn-home btn btn-secondary px-4 text-white d-flex align-items-center">
+						<span class="ti-angle-left pr-2"></span>
+						back to home
+					</a>
+					<ul class="custom-breadcrumb breadcrumb mb-0 align-items-center">
+						<li class="breadcrumb-item">
+							<a href="#" class="text-white">
+								<span class="ti-home pr-2"></span>
+								Home
+							</a>
+						</li>
+						<li class="breadcrumb-item">
+							<a href="#" class="text-white">
+								Blog
+							</a>
+						</li>
+						<li class="breadcrumb-item active text-white">Blog single</li>
+					</ul>
+				</div>
+				<div class="mx-4 py-5 text-white">
+					<h1 class="mb-4 font-weight-normal">Blog Page</h1>
+					<span>Sam Martin Blog</span>
+				</div>
+			</div>
+		</div>
+		<div class="blog-desc py-5 mx-4">
+			<div class="row mx-0">
+				<div class="col-xl-9 col-lg-8">
+					<div class="slider-img" style="background-image: url('../../../images/blog/09.jpg');"></div>
+					<div class="py-3">
+						<p class="font-weight-bold">The first thing to remember about success is that it is a process – nothing more, nothing less. There is really no magic to it and it’s not reserved only for a select few people. As such, success really has nothing to do with luck, coincidence or fate. </p>
+						<p class="text-gray-light">You will begin to realise why this exercise is called the Dickens Pattern (with reference to the ghost showing Scrooge some different futures) as you notice that the idea of this exercise is to hypnotize yourself to be aware of two very real possibilities for your future. Two distinct pathways that you could take for your life this very day. Have a think about something that you do that you are maybe not motivated to change about yourself. Prior to running through this technique, just have a think about something that you know you need to be doing, but are not. Then with that thing in mind, follow these simple steps. Step One: Get yourself nice and relaxed and settled. </p>
+						<blockquote class="blockquote-style px-4 py-3 border-left border-primary text-gray-light font-italic">
+							Designing is a matter of concentration. You go deep into what you want to do. It's about intensive research, really. The concentration is warm and intimate and like the fire inside the earth - intense but not distorted. You can go to a place, really feel it in your heart. It's actually a beautiful feeling.
+							<span class="d-block text-primary pt-3">- Peter Zumthor</span>
+						</blockquote>
+						<p class="text-gray-light">Really experience that. See what you see, hear what you hear, feel the feelings. Disappointment? Anger? Frustration? Failure? How does that feel? How do you affect those around you? How do they feel? Absorb every aspect of this path that you can take today if you so choose. Notice everything that you need to know about what it will be like if you carry on with the same behaviour, putting off change. Drift back to where the path splits. Step Five: Now take a step out on to the path to the right. This is where you create powerful, progressive change. Notice the sense of freedom in your thoughts, the sense of accomplishment and walk out 10 years into your future.</p>
+						<p class="text-gray-light">Imagine you are 10 years into the future but this time it’s different. Why? Because starting today you actually begin making changes in your life. Specific intentional changes are not easy. They are intentional because these changes are changes that you are choosing and they are the changes that will cause you to live the life you want to live and dream. They often mean leaving the perception of security in order to discover your personal freedom. These are the changes that will bring happiness and satisfaction into your life. Just go there now. 10 years out… having made a decade of changes. Imagine living the life you want to live. How does that feel inside? Do you feel that you have lived life? See the people of your life and how they feel about you and how they react to you. This is the path of a different choice, a different decision.</p>
+					</div>
+					<div class="blog-comments py-4">
+						<div class="comments-block my-3 d-sm-table w-100">
+							<div class="comments-photo d-sm-table-cell">
+								<img src="../../../images/testimonials/04.jpg" class="mw-100 mb-3">
+							</div>
+							<div class="d-sm-table-cell align-top">
+								<div class="bg-light p-3">
+									<h6 class="text-primary font-weight-normal d-inline-block"> Michael Bean
+									<span class="text-dark small">Sep 15, 2016</span>
+									</h6>
+									<div class="float-right">
+										<a href="#">Reply</a>
+									</div>
+									<p class="text-gray-light">Imagine you are 10 years into the future but this time it’s different. Why? Because starting today you actually begin making changes in your life. Specific intentional changes are not easy. They are intentional because these changes are changes that you are choosing and they are the changes that will cause you to live the life you want to live and dream.</p>
+								</div>
+								<div class="comments-block my-3 d-sm-table w-100">
+									<div class="comments-photo d-sm-table-cell">
+										<img src="../../../images/testimonials/03.jpg" class="mw-100 mb-3">
+									</div>
+									<div class="d-sm-table-cell align-top">
+										<div>
+											<h6 class="text-primary font-weight-normal d-inline-block"> Michael Bean
+											<span class="text-dark small">Sep 15, 2016</span>
+											</h6>
+											<div class="float-right">
+												<a href="#">Reply</a>
+											</div>
+											<p class="text-gray-light">Imagine you are 10 years into the future but this time it’s different. Why? Because starting today you actually begin making changes in your life. Specific intentional changes are not easy. They are intentional because these changes are changes that you are choosing and they are the changes that will cause you to live the life you want to live and dream.</p>
+										</div>
+									</div>
+								</div>
+								<div class="comments-block my-3 d-sm-table w-100">
+									<div class="comments-photo d-sm-table-cell">
+										<img src="../../../images/testimonials/05.jpg" class="mw-100 mb-3">
+									</div>
+									<div class="d-sm-table-cell align-top">
+										<div>
+											<h6 class="text-primary font-weight-normal d-inline-block"> Michael Bean
+											<span class="text-dark small">Sep 15, 2016</span>
+											</h6>
+											<div class="float-right">
+												<a href="#">Reply</a>
+											</div>
+											<p class="text-gray-light">Imagine you are 10 years into the future but this time it’s different. Why? Because starting today you actually begin making changes in your life. Specific intentional changes are not easy. They are intentional because these changes are changes that you are choosing and they are the changes that will cause you to live the life you want to live and dream.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="comments-block my-3 d-sm-table w-100">
+							<div class="comments-photo d-sm-table-cell">
+								<img src="../../../images/testimonials/02.jpg" class="mw-100 mb-3">
+							</div>
+							<div class="d-sm-table-cell align-top">
+								<div class="bg-light p-3">
+									<h6 class="text-primary font-weight-normal d-inline-block"> Michael Bean
+									<span class="text-dark small">Sep 15, 2016</span>
+									</h6>
+									<div class="float-right">
+										<a href="#">Reply</a>
+									</div>
+									<p class="text-gray-light">Imagine you are 10 years into the future but this time it’s different. Why? Because starting today you actually begin making changes in your life. Specific intentional changes are not easy. They are intentional because these changes are changes that you are choosing and they are the changes that will cause you to live the life you want to live and dream.</p>
+								</div>
+								<div class="comments-block my-3 d-sm-table w-100">
+									<div class="comments-photo d-sm-table-cell">
+										<img src="../../../images/testimonials/03.jpg" class="mw-100 mb-3">
+									</div>
+									<div class="d-sm-table-cell align-top">
+										<div>
+											<h6 class="text-primary font-weight-normal d-inline-block"> Michael Bean
+											<span class="text-dark small">Sep 15, 2016</span>
+											</h6>
+											<div class="float-right">
+												<a href="#">Reply</a>
+											</div>
+											<p class="text-gray-light">Imagine you are 10 years into the future but this time it’s different. Why? Because starting today you actually begin making changes in your life. Specific intentional changes are not easy. They are intentional because these changes are changes that you are choosing and they are the changes that will cause you to live the life you want to live and dream.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-4"></div>
+			</div>
+		</div>
+	</main>
+	<?php include_once '../../inc/footer/footer.php';?>
+</div>
+</body>
+</html>
