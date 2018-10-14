12449d8d7c0801d94f00128028fdea34a49ac87d
Janki Gandhi <jankigandhi@atyantik.com>
2018-10-14 22:00:57 +0530

    designing home page

--- madina-pay/pages/home/home.php
+++ madina-pay/pages/home/home.php
@@ -1,24 +1,202 @@
 <?php include_once '../../inc/head.php';?>
 </head>
 <body>
-<header class="site-header">
-	<div class="container">
-		<div class="header-bar">
-			<div class="float-left">
-				<div class="logo">
-					<h1 class="mb-0">
-						<a href="#">Logo</a>
-					</h1>
+<div id="page">
+	<header class="site-header">
+		<div class="container">
+			<div class="header-bar">
+				<div>
+					<div class="logo">
+						<a href="#" class="h2">Logo</a>
+					</div>
+				</div>
+				<div>
+					<a href="#" class="login-btn button button-primary-text mb-0">Login</a>
+					<a href="#" class="signup-btn button button-primary mb-0">
+						Sign Up
+						<i class="fa fa-arrow-right"></i>
+					</a>
 				</div>
 			</div>
-			<div class="float-right">
-				<a href="#" class="button button-primary-text">Login</a>
-				<a href="#" class="button button-primary">Sign Up</a>
+		</div>
+	</header>
+	<main class="home-page">
+		<div class="banner">
+			<div class="container">
+				<div class="banner-content row">
+					<div class="col col-md-6">
+						<div class="heading-section">
+							<h1 class="title pr-3">Finest approach to collect
+							payments on your website or app
+							<span class="text-primary">From as low as 1.5%</span>
+							</h1>
+							<p class="desc">
+								With the simplest payment integration, entirely online onboarding, feature filled checkout and best in class performance, quickly go live with Madina Payment Gateway in less than 2 hours.
+								<small class="mt-3">Get more than just a payment checkout. No setup charge. No maintenance fee. Only pay when you receive!</small>
+							</p>
+						</div>
+						<form>
+							<div class="form-control">
+								<input type="text" placeholder="Your email address"  class="input-box" />
+								<button class="btn-get button button-primary-text mb-0">
+								Get
+								<i class="fa fa-arrow-right"></i>
+								</button>
+							</div>
+						</form>
+						<a href="#" class="button button-primary-text pl-0 mb-0">View API Documentation</a>
+					</div>
+					<div class="col col-md-6 display-md-up">
+						<div class="banner-right-img" style="background-image: url('../../images/banner.png');"></div>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-</header>
-<main></main>
+		<div class="align-center layer-1 py-4">
+			<div class="container">
+				<div class="content">
+					<h3>Madina Payment Gateway supports all the popular payment methods in Tanzania</h3>
+					<p class="desc">
+						We provide you with the freedom for your customers to choose the convenient payment method for your customers
+						<span>Get started within minutes.</span>
+					</p>
+					<div class="logo-section row my-3">
+						<div class="col col-md-4 py-3">
+							<div class="logo-img">
+								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+							</div>
+						</div>
+						<div class="col col-md-4 py-3">
+							<div class="logo-img">
+								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+							</div>
+						</div>
+						<div class="col col-md-4 py-3">
+							<div class="logo-img">
+								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+							</div>
+						</div>
+						<div class="col col-md-4 py-3">
+							<div class="logo-img">
+								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+							</div>
+						</div>
+						<div class="col col-md-4 py-3">
+							<div class="logo-img">
+								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+							</div>
+						</div>
+						<div class="col col-md-4 py-3">
+							<div class="logo-img">
+								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+							</div>
+						</div>
+					</div>
+					<div class="align-center my-3">
+						<a href="https://madinapay.co.tz/homes/front_registration" class="button button-primary-outlined mb-0" target="_blank">
+							Sign Up Now
+							<i class="fa fa-arrow-right"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="layer-2 py-4">
+			<div class="container align-center">
+				Layer2
+			</div>
+		</div>
+		<div class="layer-3 py-4">
+			<div class="container">
+				<h3 class="layer-title align-center">Get started in minutes!</h3>
+				<div class="row my-3">
+					<div class="col col-md-4 py-2">
+						<div class="block">
+							<div class="icon pr-3">
+								<i class="fa fa-check-circle"></i>
+							</div>
+							<div class="block-content">
+								<h5 class="block-title">1.Register</h5>
+								<p class="block-desc">Fill our online form and we will call you back to verify your payment gateway.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col col-md-4 py-2">
+						<div class="block">
+							<div class="icon pr-3">
+								<i class="fa fa-check-circle"></i>
+							</div>
+							<div class="block-content">
+								<h5 class="block-title">2.Authenticate</h5>
+								<p class="block-desc">Sign our agreement and return it back to us to receive your API details for integration.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col col-md-4 py-2">
+						<div class="block">
+							<div class="icon pr-3">
+								<i class="fa fa-check-circle"></i>
+							</div>
+							<div class="block-content">
+								<h5 class="block-title">3.Get Started</h5>
+								<p class="block-desc">You are now ready to transact! Start receiving payments online using our secure gateway.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="align-center my-3">
+					<a href="https://madinapay.co.tz/homes/front_registration" class="button button-primary-outlined mb-0" target="_blank">
+						Sign Up Now
+						<i class="fa fa-arrow-right"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="layer-4 py-4">
+			<div class="container align-center">
+				Layer4
+			</div>
+		</div>
+		<div class="layer-5 py-4">
+			<div class="container align-center">
+				<h3>Join over 620+ online businesses</h3>
+				<a href="#" class="button button-primary my-3">
+					Register Now
+					<i class="fa fa-arrow-right"></i>
+				</a>
+				<div class="companies-logo">
+					<div class="logo-img">
+						<img src="../../images/c-logo.png" alt="company-logo" />
+					</div>
+					<div class="logo-img">
+						<img src="../../images/c-logo.png" alt="company-logo" />
+					</div>
+					<div class="logo-img">
+						<img src="../../images/c-logo.png" alt="company-logo" />
+					</div>
+					<div class="logo-img">
+						<img src="../../images/c-logo.png" alt="company-logo" />
+					</div>
+					<div class="logo-img">
+						<img src="../../images/c-logo.png" alt="company-logo" />
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="layer-6 py-4">
+			<div class="container align-center">
+				<div class="content">
+					<h3>Bank Level Security</h3>
+					<p class="desc">We provide a secure online payment gateway environment using 256-bit encryption.
+					BOT regulations compliant.</p>
+					<div class="security-logo align-center">
+						<img src="../../images/tanzania.gif" alt="Tanzania" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
 <footer></footer>
+</div>
 </body>
 </html>
