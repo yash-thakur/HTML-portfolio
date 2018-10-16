5fead98ff9f03eb3337aa8a7bed86ca564240dd6
Janki Gandhi <jankigandhi@atyantik.com>
2018-10-17 00:19:40 +0530

    designing home page

--- madina-pay/pages/home/home.php
+++ madina-pay/pages/home/home.php
@@ -7,12 +7,14 @@
 			<div class="header-bar">
 				<div>
 					<div class="logo">
-						<a href="#" class="h2">Logo</a>
+						<a href="#" class="h2">
+							<img src="../../images/mpg-logo.png" alt="Madinapay" />
+						</a>
 					</div>
 				</div>
 				<div>
-					<a href="#" class="login-btn button button-primary-text mb-0">Login</a>
-					<a href="#" class="signup-btn button button-primary mb-0">
+					<a href="https://madinapay.co.tz/merchants" class="login-btn button button-primary-text mb-0">Login</a>
+					<a href="https://madinapay.co.tz/homes/front_registration" class="signup-btn button button-primary mb-0">
 						Sign Up
 						<i class="fa fa-arrow-right"></i>
 					</a>
@@ -35,19 +37,23 @@
 								<small class="mt-3">Get more than just a payment checkout. No setup charge. No maintenance fee. Only pay when you receive!</small>
 							</p>
 						</div>
-						<form>
-							<div class="form-control">
-								<input type="text" placeholder="Your email address"  class="input-box" />
-								<button class="btn-get button button-primary-text mb-0">
-								Get
-								<i class="fa fa-arrow-right"></i>
-								</button>
-							</div>
+						<!-- <form>
+										<div class="form-control">
+														<input type="text" placeholder="Your email address"  class="input-box" />
+														<button class="btn-get button button-primary-text mb-0">
+														Get
+														<i class="fa fa-arrow-right"></i>
+														</button>
+										</div>
 						</form>
-						<a href="#" class="button button-primary-text pl-0 mb-0">View API Documentation</a>
+						<a href="#" class="button button-primary-text pl-0 mb-0">View API Documentation</a> -->
+						<a href="https://madinapay.co.tz/homes/front_registration" class="button button-primary mb-0">
+							Sign Up
+							<i class="fa fa-arrow-right"></i>
+						</a>
 					</div>
 					<div class="col col-md-6 display-md-up">
-						<div class="banner-right-img" style="background-image: url('../../images/banner.png');"></div>
+						<!-- <div class="banner-right-img" style="background-image: url('../../images/banner.png');"></div> -->
 					</div>
 				</div>
 			</div>
@@ -58,43 +64,54 @@
 					<h3>Madina Payment Gateway supports all the popular payment methods in Tanzania</h3>
 					<p class="desc">
 						We provide you with the freedom for your customers to choose the convenient payment method for your customers
-						<span>Get started within minutes.</span>
+						<a href="#" class="small">Get started within minutes.</a>
 					</p>
 					<div class="logo-section row my-3">
-						<div class="col col-md-4 py-3">
+						<div class="col col-md-3 col-sm-6 py-3">
 							<div class="logo-img">
 								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
 							</div>
 						</div>
-						<div class="col col-md-4 py-3">
+						<div class="col col-md-3 col-sm-6 py-3">
 							<div class="logo-img">
-								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+								<img src="../../images/airtel-money.png" alt="Airtel Money" />
 							</div>
 						</div>
-						<div class="col col-md-4 py-3">
+						<div class="col col-md-3 col-sm-6 py-3">
 							<div class="logo-img">
-								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+								<img src="../../images/tigo-pesa.png" alt="Tigo pesa" />
 							</div>
 						</div>
-						<div class="col col-md-4 py-3">
+						<div class="col col-md-3 col-sm-6 py-3">
 							<div class="logo-img">
-								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+								<img src="../../images/halo-pesa.png" alt="Halo pesa" />
 							</div>
 						</div>
-						<div class="col col-md-4 py-3">
+						<div class="col col-md-3 col-sm-6 py-3">
 							<div class="logo-img">
-								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+								<img src="../../images/ezy-pesa.png" alt="Ezy pesa" />
 							</div>
 						</div>
-						<div class="col col-md-4 py-3">
+						<div class="col col-md-3 col-sm-6 py-3">
 							<div class="logo-img">
-								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+								<img src="../../images/nmb-wakala.png" alt="NMB Wakala" />
+							</div>
+						</div>
+						<div class="col col-md-3 col-sm-6 py-3">
+							<div class="logo-img">
+								<img src="../../images/nmb.png" alt="NMB Bank" />
+							</div>
+						</div>
+						<div class="col col-md-3 col-sm-6 py-3">
+							<div class="logo-img">
+								<img src="../../images/nmb-mobile.png" alt="NMB Mobile" />
 							</div>
 						</div>
 					</div>
 					<div class="align-center my-3">
-						<a href="https://madinapay.co.tz/homes/front_registration" class="button button-primary-outlined mb-0" target="_blank">
-							Sign Up Now
+						<h6>Experience a demo payment</h6>
+						<a href="https://madinapay.co.tz/homes/front_registration" class="button button-primary mb-0" target="_blank">
+							Pay
 							<i class="fa fa-arrow-right"></i>
 						</a>
 					</div>
@@ -102,8 +119,18 @@
 			</div>
 		</div>
 		<div class="layer-2 py-4">
-			<div class="container align-center">
-				Layer2
+			<div class="container">
+				<div class="row">
+					<div class="col col-md-6 align-center">
+						<img src="../../images/default-img.png" alt="Default Image" />
+					</div>
+					<div class="col col-md-6">
+						<h4 class="mb-2">Money in your Preferred Bank Account</h4>
+						<p class="desc m-0">
+							Link your online payment gateway account to any Bank Account in Tanzania by just providing your <a href="#">Bank Name.</a> Account Name & Number. You will get the money in your account automatically once the transaction is processed.
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="layer-3 py-4">
@@ -159,26 +186,53 @@
 		</div>
 		<div class="layer-5 py-4">
 			<div class="container align-center">
-				<h3>Join over 620+ online businesses</h3>
-				<a href="#" class="button button-primary my-3">
-					Register Now
-					<i class="fa fa-arrow-right"></i>
-				</a>
-				<div class="companies-logo">
-					<div class="logo-img">
-						<img src="../../images/c-logo.png" alt="company-logo" />
-					</div>
-					<div class="logo-img">
-						<img src="../../images/c-logo.png" alt="company-logo" />
-					</div>
-					<div class="logo-img">
-						<img src="../../images/c-logo.png" alt="company-logo" />
-					</div>
-					<div class="logo-img">
-						<img src="../../images/c-logo.png" alt="company-logo" />
-					</div>
-					<div class="logo-img">
-						<img src="../../images/c-logo.png" alt="company-logo" />
+				<div class="content">
+					<h3>Join over 620+ online businesses</h3>
+					<a href="#" class="button button-primary my-3">
+						Register Now
+						<i class="fa fa-arrow-right"></i>
+					</a>
+					<div class="logo-section row my-3">
+						<div class="col col-md-3 col-sm-6 py-3">
+							<div class="logo-img">
+								<img src="../../images/m-pesa-logo.png" alt="M-pesa" />
+							</div>
+						</div>
+						<div class="col col-md-3 col-sm-6 py-3">
+							<div class="logo-img">
+								<img src="../../images/airtel-money.png" alt="Airtel Money" />
+							</div>
+						</div>
+						<div class="col col-md-3 col-sm-6 py-3">
+							<div class="logo-img">
+								<img src="../../images/tigo-pesa.png" alt="Tigo pesa" />
+							</div>
+						</div>
+						<div class="col col-md-3 col-sm-6 py-3">
+							<div class="logo-img">
+								<img src="../../images/halo-pesa.png" alt="Halo pesa" />
+							</div>
+						</div>
+						<div class="col col-md-3 col-sm-6 py-3">
+							<div class="logo-img">
+								<img src="../../images/ezy-pesa.png" alt="Ezy pesa" />
+							</div>
+						</div>
+						<div class="col col-md-3 col-sm-6 py-3">
+							<div class="logo-img">
+								<img src="../../images/nmb-wakala.png" alt="NMB Wakala" />
+							</div>
+						</div>
+						<div class="col col-md-3 col-sm-6 py-3">
+							<div class="logo-img">
+								<img src="../../images/nmb.png" alt="NMB Bank" />
+							</div>
+						</div>
+						<div class="col col-md-3 col-sm-6 py-3">
+							<div class="logo-img">
+								<img src="../../images/nmb-mobile.png" alt="NMB Mobile" />
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -196,7 +250,51 @@
 			</div>
 		</div>
 	</main>
-<footer></footer>
+<footer class="site-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col col-md-4 col-sm-6">
+				<h4 class="footer-title">Company</h4>
+				<div>
+					<p class="mt-0 mb-2">
+						<a href="https://www.payumoney.com/aboutus.html">About Us</a>
+					</p>
+					<p class="mt-0 mb-2">
+						<a href="https://www.payumoney.com/privacypolicy.html">Privacy Policy</a>
+					</p>
+					<p class="mt-0 mb-2">
+						<a href="https://www.payumoney.com/tnc.html">Terms of Service</a>
+					</p>
+				</div>
+			</div>
+			<div class="col col-md-4 col-sm-6">
+				<h4 class="footer-title">Platform</h4>
+				<div>
+					<p class="mt-0 mb-2">
+						<a href="#">API Documentation</a>
+					</p>
+					<p class="mt-0 mb-2">
+						<a href="#">Features</a>
+					</p>
+					<p class="mt-0 mb-2">
+						<a href="#">Pricing</a>
+					</p>
+				</div>
+			</div>
+			<div class="col col-md-4 col-sm-6">
+				<h4 class="footer-title">Resources</h4>
+				<div>
+					<p class="mt-0 mb-2">
+						<a href="#">Demo Payment</a>
+					</p>
+					<p class="mt-0 mb-2">
+						<a href="#">Help Center</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
 </div>
 </body>
 </html>
